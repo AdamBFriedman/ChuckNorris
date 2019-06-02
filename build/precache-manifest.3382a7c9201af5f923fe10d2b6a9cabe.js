8bd43b1b6577d5a13f29b32d00de3dba4e478ec5
Adam Friedman <49768787+AdamBFriedman@users.noreply.github.com>
2019-06-01 19:00:59 -0700

    Initial Commit

---
+++ build/precache-manifest.3382a7c9201af5f923fe10d2b6a9cabe.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02612b34c4501b81e351a0967f3f81d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf36747ed69bb05fd96f",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "89a4ecdc1e5d764ba2f1",
+    "url": "/static/css/main.1a1191bc.chunk.css"
+  },
+  {
+    "revision": "bf36747ed69bb05fd96f",
+    "url": "/static/js/2.3bc6e94b.chunk.js"
+  },
+  {
+    "revision": "89a4ecdc1e5d764ba2f1",
+    "url": "/static/js/main.22866ddd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e31b37e95a385cb264db68c73ae617c0",
+    "url": "/static/media/chuckKarate.e31b37e9.jpg"
+  },
+  {
+    "revision": "21de593bf074d519fba69fe9dcc16b41",
+    "url": "/static/media/chuckNorris.21de593b.jpg"
+  },
+  {
+    "revision": "a8293006a1cb102ce1b8e9e9177f4373",
+    "url": "/static/media/forceOfOne.a8293006.jpg"
+  },
+  {
+    "revision": "907de119ba17e607386756084e492e87",
+    "url": "/static/media/logansWar.907de119.jpg"
+  },
+  {
+    "revision": "966a8fafa90c98e69398da27f783f4cb",
+    "url": "/static/media/missingInAction.966a8faf.jpg"
+  },
+  {
+    "revision": "22a91483446ea449af449f42089b7621",
+    "url": "/static/media/movieScreen.22a91483.jpg"
+  },
+  {
+    "revision": "5b98c81b230e2ee03aca45e8f2c511fb",
+    "url": "/static/media/octagon.5b98c81b.jpg"
+  },
+  {
+    "revision": "c86f5832245cd3389dad860fcd5d6494",
+    "url": "/static/media/sidekicks.c86f5832.jpg"
+  },
+  {
+    "revision": "265efcab2cae997072698dc45c4e9ee9",
+    "url": "/static/media/walker.265efcab.png"
+  }
+]);
